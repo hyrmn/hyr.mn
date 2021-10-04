dcfbcec437cc8fb99e519e0fde67e2eb112614ee
Ben Hyrman <ben@hyr.mn>
2021-10-04 12:16:00 -0400

    Turns out I'm now using default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,10 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: [
     './src/**/*.njk',
   ],
   darkMode: false, // or 'media' or 'class',
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      blue: colors.blue,
-      yellow: colors.amber,
-      gray: colors.gray,
-      green: colors.green
-    },    
+
     extend: {
       screens: {
         'print': {'raw': 'print'},
