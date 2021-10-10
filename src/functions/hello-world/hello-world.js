4c3f659e3914ba27a6282538390bd3bf8078bd53
Ben Hyrman <ben@hyr.mn>
2021-10-10 18:25:52 -0400

    Try out .env file

--- src/functions/hello-world/hello-world.js
+++ src/functions/hello-world/hello-world.js
@@ -7,9 +7,6 @@ const handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject} - ${thing}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
